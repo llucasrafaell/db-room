4635f6d2dc7033a1601612ab37d3c5f167795c8f
Lucas Martins <lucasmartins.ufrgs@gmail.com>
2022-04-05 13:59:56 -0300

    Mudança de pagina e posição dos modelos 3D

--- components/Grid3d.js
+++ components/Grid3d.js
@@ -30,22 +30,24 @@ export default function Grid3d(props) {
                 />
                 <Suspense fallback={null}>
                     <Mesa
-                    position = {[0,-65,55]}
+                    position = {[50,-55,-70]}
                     rotation = {[dToR(180),dToR(0),dToR(0)]}
                     />
                     <Casa
                     rotation = {[dToR(180),dToR(0),dToR(0)]}
                     />
                     <Logo
-                     position = {[-110,-110,-10]}
+                     position = {[-110,20, -115]}
                      rotation = {[dToR(180),dToR(0),dToR(0)]}
                     />
                     <Home
-                     position = {[150,-70,30]}
+                     position = {[200,-30,90]}
                      rotation = {[dToR(180),dToR(0),dToR(0)]}
                     />
                     <Laptop
                         scale = {15}
+                        position = {[50,-30,-90]}
+                        rotation = {[dToR(0),dToR(90),dToR(0)]}
                     />
                 </Suspense>
                 <Controls/>
